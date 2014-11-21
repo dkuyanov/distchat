556d1079b68aeaa5eed6a14384ef243264ea9722
dkuyanov <dmitry.kuyanov@gmail.com>
2014-11-21 10:54:22 +0600

    Initial

---
+++ distchat/distchat/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace distchat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
